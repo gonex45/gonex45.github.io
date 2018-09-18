d0dfb464054b8e70274cdc75f4a48ffe7d893d1d
gonex45 <gonex@astrocorp.com.tw>
2018-09-18 12:13:23 +0800

    Site updated: 2018-09-18 12:13:21

--- js/script.js
+++ js/script.js
@@ -44,7 +44,8 @@ function Gsitesearch(curobj){
         var d = document, s = d.createElement('script');
 
         // IMPORTANT: Replace EXAMPLE with your forum shortname!
-        s.src = 'https://blog.gorendo.disqus.com/embed.js';
+                 https://blog.gorendo.disqus.com/embed.js
+        s.src = 'http://blog-gorendo.disqus.com/embed.js';
 
         s.setAttribute('data-timestamp', +new Date());
         (d.head || d.body).appendChild(s);
