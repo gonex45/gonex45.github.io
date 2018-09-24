81140f86326b75a87e47a0abccec186529a2d97e
gonex45 <gonex@astrocorp.com.tw>
2018-09-24 14:54:34 +0800

    Site updated: 2018-09-24 14:54:33

--- js/script.js
+++ js/script.js
@@ -1,48 +1,46 @@
-function ready(fn) {
-  if (document.attachEvent ? document.readyState === "complete" : document.readyState !== "loading"){
+function onReady(fn) {
+  if (document.attachEvent ? document.readyState === "complete" : document.readyState !== "loading") {
     fn();
   } else {
     document.addEventListener('DOMContentLoaded', fn);
   }
 }
 
-function setMenuBtn() {
-    // $(document).ready(function () {
-      //   $('[data-toggle="offcanvas"]').click(function () {
-      //     $('#aside').toggleClass('active')
-      //   });
-      // });
-      var asidebtn = document.getElementById('asidebtn'); // or whatever triggers the toggle
+onReady(function () {
+  var asidebtn = document.getElementById('asidebtn'); // or whatever triggers the toggle
 
-      asidebtn.addEventListener('click', function(e) {
-        var aside = document.getElementById('aside');
-        aside.classList.toggle('active');
-        asidebtn.classList.toggle('active');
-      });
-}
-ready(setMenuBtn);
-console.log("???");
+  asidebtn.addEventListener('click', function (e) {
+    var aside = document.getElementById('aside');
+    aside.classList.toggle('active');
+    asidebtn.classList.toggle('active');
+    aside.classList.remove('noactived');
+  });
+});
 
 
-function Gsitesearch(curobj){
-  curobj.q.value="site:"+window.location.hostname+" "+curobj.qfront.value;
+function Gsitesearch(curobj) {
+  curobj.q.value = "site:" + window.location.hostname + " " + curobj.qfront.value;
 }
 
-    /**
-     *  RECOMMENDED CONFIGURATION VARIABLES: EDIT AND UNCOMMENT
-     *  THE SECTION BELOW TO INSERT DYNAMIC VALUES FROM YOUR
-     *  PLATFORM OR CMS.
-     *
-     *  LEARN WHY DEFINING THESE VARIABLES IS IMPORTANT:
-     *  https://disqus.com/admin/universalcode/#configuration-variables
-     */
 
-    (function() {  // REQUIRED CONFIGURATION VARIABLE: EDIT THE SHORTNAME BELOW
-        var d = document, s = d.createElement('script');
+// disqus
 
-        // IMPORTANT: Replace EXAMPLE with your forum shortname!
-        s.src = window.location.protocol + '//blog-gorendo.disqus.com/embed.js';
+/**
+ *  RECOMMENDED CONFIGURATION VARIABLES: EDIT AND UNCOMMENT
+ *  THE SECTION BELOW TO INSERT DYNAMIC VALUES FROM YOUR
+ *  PLATFORM OR CMS.
+ *
+ *  LEARN WHY DEFINING THESE VARIABLES IS IMPORTANT:
+ *  https://disqus.com/admin/universalcode/#configuration-variables
+ */
 
-        s.setAttribute('data-timestamp', +new Date());
-        (d.head || d.body).appendChild(s);
-    })();
+(function () { // REQUIRED CONFIGURATION VARIABLE: EDIT THE SHORTNAME BELOW
+  var d = document,
+    s = d.createElement('script');
+
+  // IMPORTANT: Replace EXAMPLE with your forum shortname!
+  s.src = '//blog-gorendo.disqus.com/embed.js';
+
+  s.setAttribute('data-timestamp', +new Date());
+  (d.head || d.body).appendChild(s);
+})();
